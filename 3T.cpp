c3d3ccd51349543f4741ef5637e8a6a4499cce72
IvanMev <imefd@bk.ru>
2026-05-04 23:15:18 +0300

    Add files via upload

--- 3T.cpp
+++ 3T.cpp
@@ -7,20 +7,23 @@
 int main() {
     setlocale(LC_ALL, "RU");
     std::vector<int> v;
-    int n = 0;
 
-    std::cout << "Введите число элементов:  ";
-    std::cin >> n;
+    std::cout << "Введите элементы (для завершения нажмите Ctrl+Z или введите не число): ";
 
-    std::cout << "Введите элементы: ";
-    std::copy_n(std::istream_iterator<int>(std::cin), n, std::back_inserter(v));
+    std::copy(
+        std::istream_iterator<int>(std::cin),
+        std::istream_iterator<int>(),
+        std::back_inserter(v)
+    );
 
     std::map<int, int> m;
-    for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
-        m[*it]++;
-    }
+
+    std::for_each(v.begin(), v.end(), [&m](int x) {
+        m[x]++;
+        });
 
     std::cout << "Вывод: \n";
+
     for (std::map<int, int>::iterator it = m.begin(); it != m.end(); ++it) {
         std::cout << it->first << ' ' << it->second << '\n';
     }
