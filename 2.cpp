57701f89055ef60987e0ef45d8d542d118d1fe17
IvanMev <imefd@bk.ru>
2026-05-04 23:15:43 +0300

    Add files via upload

--- 2.cpp
+++ 2.cpp
@@ -2,33 +2,31 @@
 #include <deque>
 #include <iterator>
 #include <algorithm>
+
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "RU");
     deque<int> D;
-    
-    cout << "Введите элементы дека:" << endl;
+
+    cout << "Введите элементы дека (Ctrl+Z для конца):" << endl;
     copy(istream_iterator<int>(cin), istream_iterator<int>(), back_inserter(D));
 
-    int N = static_cast<int>(D.size());
+    size_t N = D.size();
     if (N == 0 || N % 2 != 0) {
         cout << "Количество элементов должно быть положительным и четным." << endl;
         return 0;
     }
 
-    deque<int>::iterator i = D.begin() + N / 2;
+    deque<int>::iterator it = D.begin() + (N / 2);
 
-    for (int k = 0; k < N / 2; ++k) {
-        i = D.insert(--i, -1);
+    for (size_t k = 0; k < N / 2; ++k) {
+        it = D.insert(it, -1);
     }
 
     cout << "Результат:" << endl;
-    for (int x : D) {
-        cout << x << ' ';
-    }
+    copy(D.begin(), D.end(), ostream_iterator<int>(cout, " "));
     cout << endl;
 
     return 0;
 }
-
